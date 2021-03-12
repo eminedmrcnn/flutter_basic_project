bf47d0c807c2d0b443e785fe8d412886c8328b1a
emine demircan <eminemecem@gmail.com>
2021-03-13 01:26:49 +0300

    .

---
+++ lib/widgets/adapative_flat_button.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class AdapativeFlatButton extends StatelessWidget {
+  final String text;
+  final Function handler;
+
+  AdapativeFlatButton(this.handler, this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isIOS
+        ? CupertinoButton(
+            child: Text(
+              text,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onPressed: handler,
+          )
+        : FlatButton(
+            textColor: Theme.of(context).primaryColor,
+            onPressed: handler,
+            child: Text(
+              text,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          );
+  }
+}
